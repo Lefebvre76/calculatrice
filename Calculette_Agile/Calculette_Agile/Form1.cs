bdfe43b68f726d7cb966d4d2f30c1f79fbf2247e
loic lefebvre <lefebvre.loic.76@gmail.com>
2016-06-17 13:43:44 +0200

    fix bug

--- Calculette_Agile/Calculette_Agile/Form1.cs
+++ Calculette_Agile/Calculette_Agile/Form1.cs
@@ -66,7 +66,7 @@ namespace Calculette_Agile
             string displayValue = DisplayTextBox.Text;
             DisplayTextBox.Text = "";
             lastIsOperand = true;
-            
+            SignTextBox.Text = tagValue;
             switch (selectedOperator)
 	        {
                 case customOperator.addition:
@@ -91,7 +91,7 @@ namespace Calculette_Agile
             string displayValue = DisplayTextBox.Text;
             DisplayTextBox.Text = "";
             lastIsOperand = true;
-
+            SignTextBox.Text = "=";
             switch (selectedOperator)
             {
                 case customOperator.addition:
